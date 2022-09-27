d0ea9b57a1586c86cf31f132c2594e5dd4df3f1a
guadalupehelou <113600082+guadalupehelou@users.noreply.github.com>
2022-09-27 18:21:40 -0300

    LUPE

--- libreria/example.cpp
+++ libreria/example.cpp
@@ -1,9 +1,45 @@
-#include "example.h"
 
-namespace foobar
+#include "example.h"
+int temperaturaspacientes(float* temperaturas) 
 {
-    int Example::getValue() const
+    int countfeber = 0;
+    int countnormal = 0;
+    int countweird = 0;
+    
+    for (int i = 0; i < 2; i++)
     {
-        return 99;
+        if (temperaturas[i] > 35.5 && temperaturas[i] <= 37.5)
+        {
+            countnormal = countnormal + 1;
+        }
+        else if (temperaturas[i] > 37.5 && temperaturas[i] < 41)
+        {
+            countfeber = countfeber + 1;
+        }
+        else
+        {
+            countweird = countweird + 1;
+        }
     }
+    
+    if (countweird != 0)
+        return -1;  //alguno de los datos ingresados son erroneos o improbables
+    else if (countnormal == 2)
+        return 1;   //ambos normal
+    else if (countfeber == 2)
+        return 0;   //ambos fiebre
+    else
+        return 2;   //uno tiene fiebre y el otro no
+}
+
+int paroimpar(int num1, int num2)
+{
+    int resultado = 0;
+    
+    resultado = num1 + num2;
+    
+    if (resultado % 2 == 0)
+        return 1;   //es par
+    else
+        return 0;   //no es par,´por lo que es impar
 }
--- unit-test/example.cpp
+++ unit-test/example.cpp
@@ -1,13 +1,31 @@
 #include "gmock/gmock.h"
-
 #include "example.h"
 
 using namespace testing;
-
-namespace foobar::tests
+TEST(funciones, temperat1)
 {
-	TEST(ExampleTests, Example) {
-		foobar::Example example{};
-        ASSERT_THAT(example.getValue(), Eq(99));
-	}
+	float *array1_=new float [2];
+
+	array1_[0] = -23.0;
+	array1_[1] = 45.0;
+
+	ASSERT_THAT(temperaturaspacientes(array1_), -1);
+	delete[] array1_;
 }
+TEST(funciones, temperat2)
+{
+	float array_[2]={0,0};
+
+	array_[0] = 41.0;
+	array_[1] = 36.0;
+
+	ASSERT_THAT(temperaturaspacientes(array_), 2);
+	delete[] array_;
+}
+TEST(funciones, parimpar)
+{
+	ASSERT_THAT(paroimpar(2, 5), 0);
+	ASSERT_THAT(paroimpar(0, 4), 1);
+}
+
+
